67ed14e6c4027ad87f669b5eb987c356889e445d
[thomasvierk] <[thomasbvierk@gmail.com]>
2022-04-03 13:16:18 -0500

    fixed test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -107,7 +107,7 @@ namespace GithubActionsLab
         public void Pow_Valid_Vierk()
         {
             Assert.AreEqual(3, Program.Power("3", "1"));
-            Assert.AreEqual(8, Program.Power("4", "2"));
+            Assert.AreEqual(16, Program.Power("4", "2"));
             Assert.AreEqual(81, Program.Power("3", "3"));
         }
 
